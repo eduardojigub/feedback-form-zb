5cd9bc7a74a4ec58f4e1ac0201e75a38e4129913
eduardojigub <eduardobrunoit@gmail.com>
2023-02-16 21:23:06 -0300

    feat: add feedbackForm component and styling

--- front-end/src/components/FeedbackForm/index.tsx
+++ front-end/src/components/FeedbackForm/index.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { setName, setEmail, setMessage } from "../../redux/feedbackSlice";
+import {
+  Container,
+  FormContainer,
+  FormInput,
+  FormTextarea,
+  SubmitButton,
+} from "./styles";
 
-function FeedbackForm() {
-  return <div>FeedbackForm</div>;
-}
+const FeedbackForm: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { name, email, message } = useAppSelector((state) => state.feedback);
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setName(e.target.value));
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setEmail(e.target.value));
+  };
+
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    dispatch(setMessage(e.target.value));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitting feedback:", { name, email, message });
+    // Make API request to submit feedback
+  };
+
+  return (
+    <Container>
+      <FormContainer onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <FormInput
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <FormTextarea
+          placeholder="Message"
+          value={message}
+          onChange={handleMessageChange}
+        />
+        <SubmitButton type="submit">Submit</SubmitButton>
+      </FormContainer>
+    </Container>
+  );
+};
 
 export default FeedbackForm;
--- front-end/src/components/FeedbackForm/styles.ts
+++ front-end/src/components/FeedbackForm/styles.ts
@@ -1,7 +1,49 @@
 import styled from 'styled-components';
 
-
-const Test = styled.form`
-    display: flex,
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 `;
 
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  background-color: #f9f9f9;
+  border-radius: 5px;
+`;
+
+export const FormInput = styled.input`
+  margin: 10px 0;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+`;
+
+export const FormTextarea = styled.textarea`
+  margin: 10px 0;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  height: 150px;
+`;
+
+export const SubmitButton = styled.button`
+  margin-top: 10px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #4CAF50;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3e8e41;
+  }
+`;
