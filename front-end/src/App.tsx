74834813df9ca0d094663c3b27fe378902258c96
eduardojigub <eduardobrunoit@gmail.com>
2023-02-16 21:17:07 -0300

    feat: add feedbackForm

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,23 @@
-import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import styled from "styled-components";
+import FeedbackForm from "./components/FeedbackForm";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <>
-    <Provider store={store}></Provider>
-    </>;
-  )
-
+    <Provider store={store}>
+      <Container>
+        <FeedbackForm />
+      </Container>
+    </Provider>
+  );
 }
 
 export default App;
