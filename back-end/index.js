5e71edffba574de2a8e594cccb105ff6905586d0
eduardojigub <eduardobrunoit@gmail.com>
2023-02-19 12:22:15 -0300

    fix: database_name

--- back-end/index.js
+++ back-end/index.js
@@ -21,7 +21,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   waitForConnections: true,
 });
 
