0a84e096e2b751af309a7ca45e5f9df2dd46b946
eduardojigub <eduardobrunoit@gmail.com>
2023-02-19 11:34:07 -0300

    feat: updating variable port

--- back-end/index.js
+++ back-end/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const PORT = 3333;
+const PORT = process.env.PORT || 5000;
 
 // Set up CORS middleware to allow requests from frontend
 app.use(cors());
